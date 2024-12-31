520d543bd1c12fdc5782805230d61e6c96a933d0
ARKaraoglu <ahmetrusenkara@gmail.com>
2024-12-31 02:03:36 -0500

    Modified: 1.two-sum.py
    Completed the follow-up challenge.

--- Grind-75/Python/1.two-sum.py
+++ Grind-75/Python/1.two-sum.py
@@ -1,9 +1,17 @@
 # @leet start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # length = len(nums)
+        # for x in range(0, length):
+        #     for y in range(x + 1, length):
+        #         if nums[x] + nums[y] == target:
+        #             return [x, y]
         length = len(nums)
+        diffList = []
         for x in range(0, length):
-            for y in range(x + 1, length):
-                if nums[x] + nums[y] == target:
-                    return [x, y]
+            diff = target - nums[x]
+            if nums[x] in diffList:
+                return (x, nums.index(diff))
+            else:
+                diffList.append(diff)
 # @leet end
